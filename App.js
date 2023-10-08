0153a68919f2ed05f604201b3b28b7393a8b3039
vrishank-03 <100521118+vrishank-03@users.noreply.github.com>
2023-10-08 16:55:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TicTacToe from './tictactoe';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+      <main>
+        <TicTacToe />
+      </main>
+    </div>
+  );
+}
+
+export default App;
